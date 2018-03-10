b207571d57ceedf9fe3399789fdef682e36665e7
Denis <denis.dylko@gmail.com>
2018-03-10 18:46:22 +0300

    .

--- app/build/generated/source/r/debug/com/example/android/photobyintent/R.java
+++ app/build/generated/source/r/debug/com/example/android/photobyintent/R.java
@@ -17,7 +17,8 @@ public final class R {
     public static final int btnIntendV=0x7f020002;
     public static final int imageView1=0x7f020003;
     public static final int textView=0x7f020004;
-    public static final int videoView1=0x7f020005;
+    public static final int textView2=0x7f020005;
+    public static final int videoView1=0x7f020006;
   }
   public static final class layout {
     public static final int main=0x7f030000;
--- app/src/main/java/com/example/android/photobyintent/PhotoIntentActivity.java
+++ app/src/main/java/com/example/android/photobyintent/PhotoIntentActivity.java
@@ -23,9 +23,33 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.VideoView;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Surface;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class PhotoIntentActivity extends Activity {
+
+	TextView tvText;
+	SensorManager sensorManager;
+	Sensor sensorAccel;
+	Sensor sensorMagnet;
+
+	StringBuilder sb = new StringBuilder();
+
+	Timer timer;
+
+	int rotation;
 
 	private static final int ACTION_TAKE_PHOTO_B = 1;
 	private static final int ACTION_TAKE_PHOTO_S = 2;
@@ -72,10 +96,13 @@ public class PhotoIntentActivity extends Activity {
 		return storageDir;
 	}
 
+	static  int pic_cnt;
+
 	private File createImageFile() throws IOException {
 		// Create an image file name
 		String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-		String imageFileName = JPEG_FILE_PREFIX + timeStamp + "_";
+		pic_cnt++;
+		String imageFileName = JPEG_FILE_PREFIX + pic_cnt + (int)(valuesResult[0]) + "_" + (int)(valuesResult[1])+ "_" + (int)(valuesResult[2]) + "_";
 		File albumF = getAlbumDir();
 		File imageF = File.createTempFile(imageFileName, JPEG_FILE_SUFFIX, albumF);
 		return imageF;
@@ -170,7 +197,13 @@ public class PhotoIntentActivity extends Activity {
 		if (mCurrentPhotoPath != null) {
 			setPic();
 			galleryAddPic();
+            TextView debug_tvText = (TextView) findViewById(R.id.textView2);
+            debug_tvText.setText(mCurrentPhotoPath);
 			mCurrentPhotoPath = null;
+
+		}else{
+//			TextView debug_tvText = (TextView) findViewById(R.id.textView2);
+//			debug_tvText.setText("error");
 		}
 
 	}
@@ -225,6 +258,12 @@ public class PhotoIntentActivity extends Activity {
 		} else {
 			mAlbumStorageDirFactory = new BaseAlbumDirFactory();
 		}
+
+        tvText = (TextView) findViewById(R.id.textView);
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorAccel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorMagnet = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
 	}
 
 	@Override
@@ -300,5 +339,126 @@ public class PhotoIntentActivity extends Activity {
 			btn.setClickable(false);
 		}
 	}
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(listener, sensorAccel, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(listener, sensorMagnet, SensorManager.SENSOR_DELAY_NORMAL);
+
+        timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        getDeviceOrientation();
+                        getActualDeviceOrientation();
+                        showInfo();
+                    }
+                });
+            }
+        };
+        timer.schedule(task, 0, 400);
+
+        WindowManager windowManager = ((WindowManager) getSystemService(Context.WINDOW_SERVICE));
+        Display display = windowManager.getDefaultDisplay();
+//        rotation = display.getRotation();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        sensorManager.unregisterListener(listener);
+        timer.cancel();
+    }
+
+    String format(float values[]) {
+        return String.format("%1$.1f\t\t%2$.1f\t\t%3$.1f", values[0], values[1], values[2]);
+    }
+
+    void showInfo() {
+        sb.setLength(0);
+        sb.append("Orientation : " + format(valuesResult))
+                .append("\nOrientation 2: " + format(valuesResult2))
+        ;
+        tvText.setText(sb);
+    }
+
+    float[] r = new float[9];
+
+    void getDeviceOrientation() {
+        SensorManager.getRotationMatrix(r, null, valuesAccel, valuesMagnet);
+        SensorManager.getOrientation(r, valuesResult);
+
+        valuesResult[0] = (float) Math.toDegrees(valuesResult[0]);
+        valuesResult[1] = (float) Math.toDegrees(valuesResult[1]);
+        valuesResult[2] = (float) Math.toDegrees(valuesResult[2]);
+        return;
+    }
+
+    float[] inR = new float[9];
+    float[] outR = new float[9];
+
+    void getActualDeviceOrientation() {
+        SensorManager.getRotationMatrix(inR, null, valuesAccel, valuesMagnet);
+        int x_axis = SensorManager.AXIS_X;
+        int y_axis = SensorManager.AXIS_Y;
+        switch (rotation) {
+            case (Surface.ROTATION_0): break;
+            case (Surface.ROTATION_90):
+                x_axis = SensorManager.AXIS_Y;
+                y_axis = SensorManager.AXIS_MINUS_X;
+                break;
+            case (Surface.ROTATION_180):
+                y_axis = SensorManager.AXIS_MINUS_Y;
+                break;
+            case (Surface.ROTATION_270):
+                x_axis = SensorManager.AXIS_MINUS_Y;
+                y_axis = SensorManager.AXIS_X;
+                break;
+            default: break;
+        }
+        SensorManager.remapCoordinateSystem(inR, x_axis, y_axis, outR);
+        SensorManager.getOrientation(outR, valuesResult2);
+        valuesResult2[0] = (float) Math.toDegrees(valuesResult2[0]);
+        valuesResult2[1] = (float) Math.toDegrees(valuesResult2[1]);
+        valuesResult2[2] = (float) Math.toDegrees(valuesResult2[2]);
+        return;
+    }
+
+    float[] valuesAccel = new float[3];
+    float[] valuesMagnet = new float[3];
+    float[] valuesResult = new float[3];
+    float[] valuesResult2 = new float[3];
+
+
+    SensorEventListener listener = new SensorEventListener() {
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            switch (event.sensor.getType()) {
+                case Sensor.TYPE_ACCELEROMETER:
+                    for (int i=0; i < 3; i++){
+                        valuesAccel[i] = event.values[i];
+                    }
+                    break;
+                case Sensor.TYPE_MAGNETIC_FIELD:
+                    for (int i=0; i < 3; i++){
+                        valuesMagnet[i] = event.values[i];
+                    }
+                    break;
+            }
+        }
+    };
+
+
 
 }
